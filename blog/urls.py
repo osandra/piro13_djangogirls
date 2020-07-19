ed86a350d009295299daaa2a1eba414f4cb9a9bb
osandra <okhw97@gmail.com>
2020-07-19 18:25:37 +0900

    애플리케이션 확장하기

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
         path('',views.post_list, name = 'post_list'),
+        #url이 post를 포함해야 함 pk다음 오는 / 는 /가 한 번 더 와야 한다는 의미
+        path('post/<int:pk>/',views.post_detail, name = 'post_detail'),
 ]
