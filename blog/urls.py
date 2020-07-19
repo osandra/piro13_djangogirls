e53f6e9bfdad8fb5f22585130146dc055a5949d5
osandra <okhw97@gmail.com>
2020-07-19 21:31:31 +0900

    Django 폼

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('',views.post_list, name = 'post_list'),
-        #url이 post를 포함해야 함 pk다음 오는 / 는 /가 한 번 더 와야 한다는 의미
-        path('post/<int:pk>/',views.post_detail, name = 'post_detail'),
+    path('', views.post_list, name='post_list'),
+    # url이 post를 포함해야 함 pk다음 오는 / 는 /가 한 번 더 와야 한다는 의미
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
